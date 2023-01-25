1c04538eb51049246cbc0ebbf079cced59a3e514
anass2002-dr <anassdermaj163@gmail.com>
2023-01-25 17:43:55 +0100

    js

---
+++ assets/js/code.js
@@ -0,0 +1,7 @@
+// var btn_menu=document.getElementById("btn-collapse-m");
+// var div_menu=document.getElementById("navbarNavAltMarkup");
+
+// var exist=false;
+// btn_menu.addEventListener('click',()=>{
+//     div_menu.className.remove("show");
+// });
