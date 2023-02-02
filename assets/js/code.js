bc4f0e6c37b3233c13c5b81a63b612b9f4c30590
anass2002-dr <anassdermaj163@gmail.com>
2023-02-03 00:29:53 +0100

    add animation

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,7 +1,1 @@
-// var btn_menu=document.getElementById("btn-collapse-m");
-// var div_menu=document.getElementById("navbarNavAltMarkup");
-
-// var exist=false;
-// btn_menu.addEventListener('click',()=>{
-//     div_menu.className.remove("show");
-// });
+//write here your js code
